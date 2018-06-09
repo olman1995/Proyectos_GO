14a0dfe73f60c20eda78f08d39dcbcf9da965604
olmancp <olmancp95@gmail.com>
2018-06-09 15:48:57 -0600

    variables

---
+++ variables/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	//las variables inician con un valor predeterminado
+	//var x,y,z int // int = 0
+	//var c string // string = ""
+	//var b bool // bool = false
+	//x := 23 tipado dinamico
+	nombre := "abc"
+	fmt.Println(nombre)
+}
